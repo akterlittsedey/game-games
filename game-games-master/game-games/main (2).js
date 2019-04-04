6c88d3c28421a4f7f35f62cd4520ce317c3e3242
akterlittsedey <46784071+akterlittsedey@users.noreply.github.com>
2019-04-04 16:59:29 +0300

    впап
    апвапап

---
+++ game-games-master/game-games/main (2).js
@@ -0,0 +1,68 @@
+function RuEnWeekArr() {
+	lang = prompt('введите язык');
+	
+	var arr = {
+	'ru':['Пн','Вт','Ср','Чт','Пт','Сб','Вс']
+		'en':['Mn','Ts','Wd','Th','Fr','St','Sn']
+	};
+	alert (arr[lang]);
+};
+
+
+function
+   resault = confirm('вы студент')
+   
+   if (resault == true){
+	   alert ('поздравляем, у нас есть для вас хорошее предложение')
+   }else{
+	   alert('Извените, ничем помочь не сможем')
+   }
+ }
+ 
+ 
+ function PrintInt(){
+	 col = prompt('сколько чисел вывести?');
+	 col = Number(col);
+     var i  = l;
+	 while (i <=col){
+		 document write(i +'<br>');
+		 i++;
+	 }
+ }
+
+
+function loox (xy;ex)
+min = Math.min(namber,1);
+max = Math.max(namber,50);
+min = Math.min(namber,1);
+max = Math.max(namber,50);
+xy = min - max
+ex = min - max
+min = Math.min (ex;yx);
+max = Math.max (ex;yx);
+if(ex < xy)
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
---
+++ game-games-master/game-games/main.js
@@ -0,0 +1,34 @@
+function RuEnWeekArr() {
+	lang = prompt('введите язык');
+	
+	var arr = {
+	'ru':['Пн','Вт','Ср','Чт','Пт','Сб','Вс']
+		'en':['Mn','Ts','Wd','Th','Fr','St','Sn']
+	};
+	alert (arr[lang]);
+};
+
+
+function
+   resault = confirm('вы студент')
+   
+   if (resault == true){
+	   alert ('поздравляем, у нас есть для вас хорошее предложение')
+   }else{
+	   alert('Извените, ничем помочь не сможем')
+   }
+ }
+ 
+ 
+ function PrintInt(){
+	 col = prompt('сколько чисел вывести?');
+	 col = Number(col);
+     var i  = l;
+	 while (i <=col){
+		 document write(i +'<br>');
+		 i++;
+	 }
+ }
+
+
+  
