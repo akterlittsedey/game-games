87c92d89752649ba8be73d11e37164df1d692fcb
akterlittsedey <46784071+akterlittsedey@users.noreply.github.com>
2019-04-09 17:14:41 +0300

    ммм
    игргш

---
+++ game-games/loox.js
@@ -0,0 +1,5 @@
+function trollonit(){
+arr = [1,2,3,4,5,6,7,8,9,10]
+alert(arr);
+alert(arr.reverse());
+};
--- game-games/main.js
+++ game-games/main.js
@@ -1,16 +1,19 @@
-function RuEnWeekArr() {
+
+
+function RuEnWeekArr(){
 	lang = prompt('введите язык');
 	
 	var arr = {
-	'ru':['Пн','Вт','Ср','Чт','Пт','Сб','Вс']
-		'en':['Mn','Ts','Wd','Th','Fr','St','Sn']
+	'ru':['Пн','Вт','Ср','Чт','Пт','Сб','Вс'],
+	
+	'en':['Mn','Ts','Wd','Th','Fr','St','Sn']
 	};
 	alert (arr[lang]);
 };
 
 
-function
-   resault = confirm('вы студент')
+function ask(){
+   resault = confirm('вы студент');
    
    if (resault == true){
 	   alert ('поздравляем, у нас есть для вас хорошее предложение')
@@ -25,10 +28,31 @@ function
 	 col = Number(col);
      var i  = l;
 	 while (i <=col){
-		 document write(i +'<br>');
+		 document.write(i +'<br>');
 		 i++;
 	 }
  }
+ 
+ function forarr(){
+	 x = promt("сколько чесел вывести");
+	 var i;
+	 for (var i=0;i < arr.lenght;i++);
+     var arr = [2,3,4,5];
+	 result = resault * arr[i];
+	 alert(result);
+ }
+     
+	 
+	 
+	 function fororr(){
+ 
+	 for (var i=0;i< arr.lenght;i++);
+     var arr = [1,2,3,4,5];
+	 document.write(arr[i]+'<br>');
+ }
+	 
+	 
+ 
 
 
   
